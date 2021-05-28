199986f5669237304d8d174f911f0951a1b54ace
Andreas Müller <schnitzeltony@gmail.com>
2021-05-28 14:10:12 +0200

    XiQNetWrapper: Offer an implementation of byteArrayToProtobuf for most clients
    Same:
    * Without changing client's code nothing no functional change is to expect
    * This allows most clients to get rid of their implementation which was copy
      & pasted all over the place
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- xiqnetwrapper.cpp
+++ xiqnetwrapper.cpp
@@ -1,6 +1,18 @@
 #include "xiqnetwrapper.h"
 #include <netmessages.pb.h>
 
+std::shared_ptr<google::protobuf::Message> XiQNetWrapper::byteArrayToProtobuf(QByteArray t_byteArray)
+{
+    ProtobufMessage::NetMessage *intermediate = new ProtobufMessage::NetMessage();
+    if(!intermediate->ParseFromArray(t_byteArray, t_byteArray.size()))
+    {
+        ProtobufMessage::NetMessage::ScpiCommand *cmd = intermediate->mutable_scpi();
+        cmd->set_command(t_byteArray.data(), t_byteArray.size());
+    }
+    std::shared_ptr<google::protobuf::Message> proto {intermediate};
+    return proto;
+}
+
 QByteArray XiQNetWrapper::protobufToByteArray(const google::protobuf::Message &t_protobufMessage)
 {
   return QByteArray(t_protobufMessage.SerializeAsString().c_str(), t_protobufMessage.ByteSizeLong());
